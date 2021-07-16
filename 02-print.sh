8d15a69aab56434c3ee4277c7e84562c78b52b00
AG <akashbtech120@gmail.com>
2021-07-16 23:02:00 +0530

    Print Something on o/p window

---
+++ 02-print.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Printing something on screen can be done by two commands
+# 1. echo
+# 2. printf
+
+# printf will have more syntax to deal with print something on command window (More Complex)
+# echo is more descriptive and yet does all the things that printf does ( Most Simple)
+
+## Syntax: echo <MESSAGE TO PRINT>
+
+echo Hello World!
+
+
+
