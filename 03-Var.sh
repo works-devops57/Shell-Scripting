aa0fa23fe727795f6b8bad4377c0dac8b5fbbdbc
AG <akashbtech120@gmail.com>
2021-07-18 17:48:02 +0530

    Current Date Substitution

--- 03-Var.sh
+++ 03-Var.sh
@@ -10,3 +10,10 @@ echo Welcome, Today date is $DATE
 # Command Subs
 NO_OF_USERS=$(who |wc -l)
 echo Numbers of Users= $NO_OF_USERS
+
+# Date with command substitution
+DATE=$(date +%F)
+echo Welcome, Today date is $DATE
+
+
+
