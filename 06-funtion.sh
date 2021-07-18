99f539f51918241fd51694127bc11cc77c5b7bb5
AG <akashbtech120@gmail.com>
2021-07-18 22:11:23 +0530

    Function Declaration in Shell Scripting

--- 06-funtion.sh
+++ 06-funtion.sh
@@ -5,8 +5,12 @@
 sample() {
   echo Hello, I am a sample function
   echo value of a = ${a}
+  b=200
+  echo First Arguments in Function = $1
 }
 
 ## Call the function
 a=100
 sample
+echo value of b=${b}
+echo First Argument in Main Program=$1
