323660e6fb5b79c777ed6667fb846e668e1426a5
AG <akashbtech120@gmail.com>
2021-07-21 16:23:08 +0530

    Append script from mongodb frontend section to IJ

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -6,9 +6,21 @@ PRINT "Installing Nginx"
 yum install nginx -y  &>>$LOG
 STAT_CHECK $?
 
-## 1. Output from command should not be displayed on screen
-## 2. Validate the command is successful or not
-## 3. Need to validate whether the script is running as root user or not
+PRINT "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old HtDocs"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
 
 PRINT "Enabling Nginx\t"
 systemctl enable nginx &>>$LOG
