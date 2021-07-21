1f23b4c848aaf151d89df29750efd7dcc631e5a7
AG <akashbtech120@gmail.com>
2021-07-21 15:24:28 +0530

    STAT-CHECK Function Calling with PRINT

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -23,10 +23,10 @@ STAT_CHECK $?
 ## 2. Validate the command is successful or not
 ## 3. Need to validate whether the script is running as root user or not
 
-PRINT "Enabling Nginx"
+PRINT "Enabling Nginx\t"
 systemctl enable nginx &>>$LOG
 STAT_CHECK $?
 
-PRINT "Starting Nginx"
+PRINT "Starting Nginx\t"
 systemctl start nginx  &>>$LOG
 STAT_CHECK $?
