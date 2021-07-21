7078d48434f5683e5689c65bdbcf54ae15d4b53a
AG <akashbtech120@gmail.com>
2021-07-21 21:15:53 +0530

    Configure Mongodb

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -27,7 +27,7 @@ PRINT " Download MongoDB Schema\t"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Load Schema\t\t\t"
+PRINT "Load Schema\t\t"
 cd /tmp && unzip -o mongodb.zip &>>$LOG && cd mongodb-main &>>$LOG && mongo < catalogue.js &>>$LOG && mongo < users.js &>>$LOG
 STAT_CHECK $?
 
