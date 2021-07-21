aada1c07940812c19ac847724a8ed0ad3f9dbf6a
AG <akashbtech120@gmail.com>
2021-07-21 15:16:21 +0530

    STAT-CHECK Function Calling

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,32 +3,27 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
-echo -n -e "Installing Nginx\t\t... "
-yumm install nginx -y  &>>$LOG
-if [ $? -eq 0 ]; then
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
 else
   echo -e "\e[32m fail\e[0m"
   exit 1
 fi
+}
+
+echo -n -e "Installing Nginx\t\t... "
+yum install nginx -y  &>>$LOG
+STAT_CHECK $?
+
 ## 1. Output from command should not be displayed on screen
 ## 2. Validate the command is successful or not
 ## 3. Need to validate whether the script is running as root user or not
 
 echo -n -e "Enabling Nginx\t\t\t... "
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[32m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
 
 echo -n -e "Starting Nginx\t\t\t... "
 systemctl start nginx  &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[32m fail\e[0m"
-  exit 1
-fi
+STAT_CHECK $?
