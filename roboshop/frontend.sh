ca1732e819c611cc1253a0bb3582fbb29afb5765
AG <akashbtech120@gmail.com>
2021-07-21 08:37:11 +0530

    Using Redirect Concept-Double Redirect

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 LOG=/tmp/roboshop.log
+rm -f $LOG
+
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y  >>$LOG
 ## 1."Output from command should not be displayed on screen"
