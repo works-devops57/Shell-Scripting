0537df5042ddff5b04288e938f5742eed8e3b232
AG <akashbtech120@gmail.com>
2021-07-21 09:54:13 +0530

    Validate the command

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -22,7 +22,7 @@ else
   echo fail
 fi
 
-echo -n -e "Starting Nginx\t\t... "
+echo -n -e "Starting Nginx\t\t\t... "
 systemctl start nginx  &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32m done\e[0m"
