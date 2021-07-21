1889247a1201191e3b7c546e3e2f2a3f8da5ef91
AG <akashbtech120@gmail.com>
2021-07-21 08:27:10 +0530

    Using Redirect Concept

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+LOG=/tmp/roboshop.log
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
-yum install nginx -y
+yum install nginx -y  >$LOG
+## 1."Output from command should not be displayed on screen"
+## 2. "Validate the command is successful or not"
+
 
 echo "Enabling Nginx"
-systemctl enable nginx
+systemctl enable nginx >$LOG
 
 echo "Starting Nginx"
-systemctl start nginx
+systemctl start nginx  >$LOG
