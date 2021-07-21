1b644c6ba409fde38575bd576846802a0e22fcf4
AG <akashbtech120@gmail.com>
2021-07-21 09:24:03 +0530

    Using Redirect Concept-Exit Status

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,13 +4,16 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
-yum install nginx -y  >>$LOG
-## 1."Output from command should not be displayed on screen"
-## 2. "Validate the command is successful or not"
-
+yum install nginx -y  &>>$LOG
+echo $?
+## 1. Output from command should not be displayed on screen
+## 2. Validate the command is successful or not
+## 3. Need to validate whether the script is running as root user or not
 
 echo "Enabling Nginx"
-systemctl enable nginx >>$LOG
+systemctl enable nginx &>>$LOG
+echo $?
 
 echo "Starting Nginx"
-systemctl start nginx  >>$LOG
+systemctl start nginx  &>>$LOG
+echo $?
