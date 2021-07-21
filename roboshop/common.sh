8a8732974fa5fe00bf9232691013e31d894188ce
AG <akashbtech120@gmail.com>
2021-07-21 15:53:09 +0530

    Checking whether user is normal user or root user

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LOG=/tmp/roboshop.log
-rm -f $LOG
-
-USER_ID=${id -u}
+USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
-  echo -e "\e[You Should be root user/sudo to run this script\e[0m"
+  echo -e "\e[31m You Should be root user/sudo to run this script\e[0m"
   exit 2
 fi
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
 
 STAT_CHECK() {
   if [ $1 -eq 0 ]; then
