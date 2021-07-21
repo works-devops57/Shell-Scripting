62de8700b9e1b92ee5f406ce256680a77959bc10
AG <akashbtech120@gmail.com>
2021-07-21 15:28:53 +0530

    Reusable Code

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-LOG=/tmp/roboshop.log
-rm -f $LOG
+source common.sh
 
-STAT_CHECK() {
-  if [ $1 -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo -e "\e[32m fail\e[0m"
-  exit 1
-fi
-}
-
-PRINT() {
-  echo -n -e "$1\t\t..."
-}
 PRINT "Installing Nginx"
 yum install nginx -y  &>>$LOG
 STAT_CHECK $?
