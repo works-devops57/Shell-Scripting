669bcee970da5127a21e2028994504ee3323ad91
AG <akashbtech120@gmail.com>
2021-07-21 09:53:27 +0530

    Validate the command

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,10 +3,10 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
-echo -n -e "Installing Nginx\t\t..."
+echo -n -e "Installing Nginx\t\t... "
 yum install nginx -y  &>>$LOG
 if [ $? -eq 0 ]; then
-  echo done
+  echo -e "\e[32m done\e[0m"
 else
   echo fail
 fi
@@ -14,10 +14,18 @@ fi
 ## 2. Validate the command is successful or not
 ## 3. Need to validate whether the script is running as root user or not
 
-echo "Enabling Nginx"
+echo -n -e "Enabling Nginx\t\t\t... "
 systemctl enable nginx &>>$LOG
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m done\e[0m"
+else
+  echo fail
+fi
 
-echo "Starting Nginx"
+echo -n -e "Starting Nginx\t\t... "
 systemctl start nginx  &>>$LOG
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m done\e[0m"
+else
+  echo fail
+fi
