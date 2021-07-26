91ac9ffdf180d830b9cc88259769f8f81828e891
AG <akashbtech120@gmail.com>
2021-07-26 23:47:29 +0530

    Configure Frontend-Final

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -22,7 +22,7 @@ PRINT "Copy Roboshop Config\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
-PRINT "Update Roboshop Config"
+PRINT "Update Roboshop Config\t"
 sed -i -e  '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $?
 
