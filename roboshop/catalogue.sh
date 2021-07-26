7ac137a3492a5a5d83d82c3bd8c80ca7bcd3475e
AG <akashbtech120@gmail.com>
2021-07-26 20:47:25 +0530

    Configure Catalogue-Fourth

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -17,8 +17,12 @@ PRINT "Download Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Extract Download Code"
-cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
+PRINT "Extract Downloaded Code\t"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue
+STAT_CHECK $?
+
+PRINT "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
 STAT_CHECK $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
