79b3aeb0045dfe77d13f02a51beadeba6d2bccf4
AG <akashbtech120@gmail.com>
2021-07-26 23:36:23 +0530

    Configure Catalogue-Final

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -29,7 +29,7 @@ PRINT "Fix Application Permisssions"
 chown roboshop:roboshop /home/roboshop -R &>>$LOG
 STAT_CHECK $?
 
-PRINT "Setup SystemD file"
+PRINT "Setup SystemD file\t"
 sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 STAT_CHECK $?
 
