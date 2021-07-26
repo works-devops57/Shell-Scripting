14b6c0d53f15c32fa0ca5f15290ac380f3330742
AG <akashbtech120@gmail.com>
2021-07-26 19:08:25 +0530

    Configure Catalogue

---
+++ roboshop/catalogue.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source common.sh
+
+PRINT "Install NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Add Application Roboshop User"
+useradd roboshop &>>$LOG
+STAT_CHECK $?
+
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
