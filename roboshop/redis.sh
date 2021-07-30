35def508d875f0509743f894daa91df30ec86b6d
AG <akashbtech120@gmail.com>
2021-07-30 19:20:24 +0530

    Redis Done

---
+++ roboshop/redis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source common.sh
+
+PRINT "Install Redis Repos"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install Redis\t"
+yum install redis -y --enablerepo= remi &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+STAT_CHECK $?
+
+PRINT "Start Redis Service"
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
